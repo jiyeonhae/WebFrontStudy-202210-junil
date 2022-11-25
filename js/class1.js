96bd4b1a3df60cb78173b4e168f19cf2adb02b04
code1218 <skjil1218@gmail.com>
2022-11-25 21:12:47 +0900

    javascript 클래스, dom, 카카오맵 js

--- js/class1.js
+++ js/class1.js
@@ -8,25 +8,49 @@ class User {
     password;
     name;
     email;
+
+    constructor(username, password, name, email) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    printName() {
+        console.log(this.name + "님의 정보");
+    }
 }
 
 class UserMain {
+    static #instance = null;
+    static getInstance() {
+        if(this.#instance == null) {
+            this.#instance = new UserMain();
+        }
+        return this.#instance;
+    }
 
     main() {
-        let user = new User();
+        let user = new User("junil", "1234", "김준일", "skjil1218@gmail.com");
 
-        user.username = "junil";
-        user.password = "1234";
-        user.name = "김준일";
-        user.email = "skjil1218@gmail.com";
+        // user.username = "junil";
+        // user.password = "1234";
+        // user.name = "김준일";
+        // user.email = "skjil1218@gmail.com";
 
         console.log(user);
+
+        localStorage.username = user.username;
+
+        console.log("username: " + localStorage.username);
+
+        user.printName();
     }   
 
 }
 
 window.onload = () => {
-    let userMain = new UserMain();
+    let userMain = UserMain.getInstance();
 
     userMain.main();
 }
